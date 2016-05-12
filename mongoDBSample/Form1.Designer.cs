d55e19d29fc96ba959376113e9d8c4c972411a4d
tfs <joyceHong0827@gmail.com>
2016-05-12 14:40:31 +0800

    使用c# 呼叫mongoDB

---
+++ mongoDBSample/Form1.Designer.cs
@@ -0,0 +1,70 @@
+﻿namespace mongoDBSample
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器
+        /// 修改這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.radGridView1 = new Telerik.WinControls.UI.RadGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.radGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radGridView1.MasterTemplate)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // radGridView1
+            // 
+            this.radGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.radGridView1.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radGridView1.Location = new System.Drawing.Point(0, 0);
+            this.radGridView1.Name = "radGridView1";
+            this.radGridView1.ReadOnly = true;
+            this.radGridView1.Size = new System.Drawing.Size(1000, 573);
+            this.radGridView1.StandardTab = true;
+            this.radGridView1.TabIndex = 0;
+            this.radGridView1.Text = "radGridView1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1000, 573);
+            this.Controls.Add(this.radGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.radGridView1.MasterTemplate)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Telerik.WinControls.UI.RadGridView radGridView1;
+
+
+    }
+}
+
---
+++ mongoDBSample/Form1.cs
@@ -0,0 +1,136 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using MongoDB.Driver.Builders;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace mongoDBSample
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                string connString = "mongodb://192.168.1.63:27017/blog";
+                MongoClient _client = new MongoClient(connString);
+                MongoServer _server = _client.GetServer();
+                _server.Connect();
+                // 連接到 db
+                MongoDatabase db = _server.GetDatabase("blog");
+                MongoCollection collection = db.GetCollection("Post");
+
+
+                //新增資料
+                BsonDocument inserData1 = new BsonDocument{
+                    {"Title","c# "},
+                    {"Body","this is c#"},
+                };
+                collection.Insert(inserData1);
+
+                                
+                ////刪除資料
+                //var query = Query<Post>.EQ(p => p.Title, "c#");
+                //collection.Remove(query);
+
+                //修改資料
+                //var query = Query<Post>.EQ(p => p.Title, "c# ");
+                //var update = Update<Post>.Set(p => p.Title, "c# update");
+                //collection.Update(query, update);
+
+                //Post postObj
+                //person.Name = "Jim";
+                ObjectId objctID= new ObjectId("54a3699d0e925b162c4e2fdd");
+
+                collection.Save(new Post()
+                {
+                     Id = objctID,
+                     Title="c# ",
+                     Body="update c# ''",
+                     CharCount=10,                  
+                });
+
+                //查詢資料
+                MongoCursor<Post> cursor = collection.FindAllAs<Post>();
+                var list = cursor.ToList();
+                radGridView1.DataSource = list.ToArray();
+
+                _server.Disconnect();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+    }
+
+
+    public class Post
+    {
+       
+        public ObjectId Id
+        {
+            get;
+            set;
+        }
+
+        public string Title
+        {
+            get;
+            set;
+        }
+
+        public string Body
+        {
+            get;
+            set;
+        }
+
+        public int CharCount
+        {
+            get;
+            set;
+        }
+
+        public IList<Comment> Comments
+        {
+            get;
+            set;
+        }
+    }
+
+
+    public class Comment
+    {
+        public DateTime TimePosted
+        {
+            get;
+            set;
+        }
+
+        public string Email
+        {
+            get;
+            set;
+        }
+
+        public string Body
+        {
+            get;
+            set;
+        }
+    }
+}
